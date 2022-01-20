d5773275b2ae2914e78c47008f85a141295355e9
Alejandro <jose.alejandro.her.ros@gmail.com>
2022-01-20 23:05:53 +0100

    Integrate main for flask

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
-from config import Config
 from flask import Flask
 from flask_pymongo import PyMongo
+
+from commands.import_amz_data import commands
+from config import Config
+from graphqls.routes import graphql_routes
 
 MONGO_HOST = Config.MONGO_HOST
 MONGO_PORT = Config.MONGO_PORT
@@ -11,3 +14,6 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = f"mongodb://{MONGO_HOST}:{MONGO_PORT}/{MONGO_DB}"  # NOQA
 mongodb_client = PyMongo(app)
 db = mongodb_client.db
+
+app.register_blueprint(commands)
+app.register_blueprint(graphql_routes)
