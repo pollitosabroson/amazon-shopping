1798c08fc55f8b178aeec6c7feea22c289014916
Alejandro <jose.alejandro.her.ros@gmail.com>
2022-01-27 23:47:17 +0100

    improvement resolvers for orders

--- src/amz_orders/resolvers.py
+++ src/amz_orders/resolvers.py
@@ -6,10 +6,10 @@ from manager.amz_manager import AmzManager
 logger = logging.getLogger(__name__)
 
 
-def resolve_single_order(_, info, where: dict = None):
+def resolver_single_order(_, info, where: dict = None):
     """Get single Order
     params:
-        where(Dcit): dict with id fikter
+        where(Dcit): dict with id filter
     """
     params = copy.deepcopy(where) or {}
     try:
@@ -25,3 +25,43 @@ def resolve_single_order(_, info, where: dict = None):
             "errors": [str(error)]
         }
     return payload
+
+
+def resolver_orders(_, info, where: dict = {}):
+    """Get filters orders
+    params:
+        where(Dcit): dict with filters params.
+    """
+    params = copy.deepcopy(where) or {}
+    if params.get('q'):
+        query_text = AmzManager.convert_params_search(
+            params.get('q')
+        )
+        params.pop('q')
+        params.update(query_text)
+    try:
+        orders = list(AmzManager.filter_values(
+            query=params
+        ))
+        payload = {
+            "success": True,
+            "orders": orders,
+            "total": calculate_total(orders)
+        }
+    except Exception as error:
+        payload = {
+            "success": False,
+            "errors": [str(error)]
+        }
+    return payload
+
+
+def calculate_total(orders):
+    """Calculate total orders
+    params:
+        orders(Collection): Collection with orders
+    """
+    total = 0
+    for order in orders:
+        total += order.get('total_owed')
+    return total
