e8b59653883a8081fb6510f3efebb29a4da26482
Alejandro <jose.alejandro.her.ros@gmail.com>
2022-01-27 23:45:19 +0100

    improvement schemas for integrate orders

--- src/graphqls/graphql.py
+++ src/graphqls/graphql.py
@@ -5,11 +5,12 @@ from ariadne import (
     snake_case_fallback_resolvers
 )
 
-from amz_orders.resolvers import resolve_single_order
+from amz_orders.resolvers import resolver_orders, resolver_single_order
 
 PATH = os.path.dirname(os.path.abspath(__file__))
 query = ObjectType("Query")
-query.set_field("Order", resolve_single_order)
+query.set_field("Order", resolver_single_order)
+query.set_field("Orders", resolver_orders)
 
 type_defs = load_schema_from_path(f'{PATH}/schema.graphql')
 
